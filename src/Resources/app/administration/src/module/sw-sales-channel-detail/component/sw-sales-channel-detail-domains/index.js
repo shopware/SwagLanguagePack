fb52a9b41c9d7443e91e11ba6e63344095e581db
Niklas Limberg <n.limberg@shopware.com>
2023-03-03 09:00:44 +0000

    LAN-135 - Create domain modal can be opened only after deleting languages

--- src/Resources/app/administration/src/module/sw-sales-channel-detail/component/sw-sales-channel-detail-domains/index.js
+++ src/Resources/app/administration/src/module/sw-sales-channel-detail/component/sw-sales-channel-detail-domains/index.js
@@ -4,7 +4,11 @@ const { Criteria } = Shopware.Data;
 Component.override('sw-sales-channel-detail-domains', {
     computed: {
         snippetSetCriteria() {
-            const locales = this.salesChannel.languages.map(language => language.locale.code);
+            const locales = this.salesChannel.languages.map(language => language.locale?.code).filter(Boolean);
+
+            if (locales.length === 0) {
+                return this.$super('snippetSetCriteria');
+            }
 
             return this.$super('snippetSetCriteria')
                 .addFilter(Criteria.equalsAny('iso', locales));

11b6bb12b527ae7beacc242869e526901e74d13c
Dominik Grothaus <d.grothaus@shopware.com>
2026-02-05 09:56:04 +0100

    fix: Remove namespace for PhpFileLoader to work

--- src/Resources/config/services_translation_decorator.php
+++ src/Resources/config/services_translation_decorator.php
@@ -1,7 +1,5 @@
 <?php declare(strict_types=1);
 
-namespace Swag\LanguagePack\Resources\config;
-
 use Swag\LanguagePack\Core\System\Snippet\Service\CleanupReplacedLanguage;
 use Swag\LanguagePack\Core\System\Snippet\Service\CleanupTranslationLoader;
 

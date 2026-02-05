f8a135773807923bd7b1261771f2df520273abcd
Dominik Grothaus <d.grothaus@shopware.com>
2026-02-05 10:09:06 +0100

    fix: Rewrite config file to Symfony 7.4 syntax

--- src/Resources/config/services_translation_decorator.php
+++ src/Resources/config/services_translation_decorator.php
@@ -6,11 +6,13 @@ use Swag\LanguagePack\Core\System\Snippet\Service\CleanupTranslationLoader;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return [
-    CleanupTranslationLoader::class => [
-        'decorates' => 'Shopware\Core\System\Snippet\Service\TranslationLoader',
-        'arguments' => [
-            service('.inner'),
-            service(CleanupReplacedLanguage::class),
+    'services' => [
+        CleanupTranslationLoader::class => [
+            'decorates' => 'Shopware\Core\System\Snippet\Service\TranslationLoader',
+            'arguments' => [
+                service('.inner'),
+                service(CleanupReplacedLanguage::class),
+            ],
         ],
     ],
 ];
